329a854ef257cad47c89cbbd1bdd5418cc3bbb60
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-07 16:03:22 +0300

    Game has many user through reviews

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  #A game has many user through reviews 
+  #This simply means might be convenient to access a list 
+  #of all the users who left reviews for a specific game 
+  #from the game instance itself 
+  has_many :users, through: :reviews
 end
