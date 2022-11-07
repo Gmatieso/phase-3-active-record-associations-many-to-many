a72990ac220f15b567fb88b0e1bdb583a3ddf0b1
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-07 16:37:50 +0300

    nothing serious

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   #user has many reviews
-  #user has many games, through review
+  #user has many games, through review.
   has_many :reviews
   has_many :games, through: :reviews
 end
